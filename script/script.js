b328726aaf6b1868d7b98e047632af06ff7a3bbc
MOEUNJI <ejm5004@naver.com>
2022-08-12 16:53:18 +0900

    aa
    aa

---
+++ script/script.js
@@ -0,0 +1,28 @@
+window.addEventListener("scroll", scroll);
+
+const scrollHeader = document.querySelector(".web-header");
+const headerMenu = document.getElementsByClassName("header-menu");
+
+function scroll() {
+  let top = window.scrollY;
+
+  console.log(top);
+  if (top >= 68) {
+    scrollHeader.classList.add("active");
+    document.querySelector(".white-logo").src = "./images/header-logo.png";
+  } else {
+    scrollHeader.classList.remove("active");
+    document.querySelector(".white-logo").src = "./images/white-logo.png";
+  }
+
+  for (let i = 0; i < headerMenu.length; i++) {
+    if (top >= 68) {
+      headerMenu[i].classList.add("on");
+    } else {
+      headerMenu[i].classList.remove("on");
+    }
+  }
+}
+
+// let header = document.querySelector('.web-header');
+//     nav = document.querySelector('')
