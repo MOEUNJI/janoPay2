fe57fa57d06742ac556358bd9eaeeb6e841d3598
MOEUNJI <ejm5004@naver.com>
2022-08-16 15:17:40 +0900

    aa
    aa

--- script/script.js
+++ script/script.js
@@ -2,6 +2,32 @@ window.addEventListener("scroll", scroll);
 
 const scrollHeader = document.querySelector(".web-header");
 const headerMenu = document.getElementsByClassName("header-main-menu");
+const mClick = document.querySelector(".m-ham-click");
+const hamBtn = document.querySelector(".ham-img");
+const mHeader = document.querySelector(".all-header");
+const close = document.querySelector(".close");
+const allContainer = document.querySelector(".all-container");
+
+hamBtn.addEventListener("click", () => {
+  if (mClick.style.display == "none") {
+    mClick.style.display = "block";
+    mHeader.style.display = "none"
+    allContainer.style.display = "none"
+  } else {
+    mClick.style.display = "none";
+  }  
+});
+
+close.addEventListener("click", () => {
+  if (mClick.style.display == "block") {
+    mClick.style.display = "none";
+    mHeader.style.display = "flex";
+    allContainer.style.display = "block";
+  } else {
+    mClick.style.display = "none";
+  }
+});
+
 
 function scroll() {
   let top = window.scrollY;
@@ -23,6 +49,3 @@ function scroll() {
     }
   }
 }
-
-// let header = document.querySelector('.web-header');
-//     nav = document.querySelector('')
